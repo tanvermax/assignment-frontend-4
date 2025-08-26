a9b2eb5c2244421616e134bdb11fd8e446444580
tanver909 <tanveermahidi@gmial.com>
2025-08-26 22:29:49 +0600

    fixes error

--- src/redux/api/baseApi.tsx
+++ src/redux/api/baseApi.tsx
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     reducerPath: "baseApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://bookmaster-liard.vercel.app/api" }),
     tagTypes: ['Book'],
     endpoints: (builder) => ({
         getBooks: builder.query({
